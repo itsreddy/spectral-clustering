b65e4df81cca9b3f99b9bc395bb2541e4e509f43
prash <dprashureddy007@gmail.com>
2020-06-27 19:30:03 -0700

    sc: preprocessed

--- spectral-clustering.py
+++ spectral-clustering.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 from sklearn.cluster import SpectralClustering 
 from sklearn.preprocessing import StandardScaler, normalize 
-import io, csv, os
+import io, csv, os, sys
 import numpy as np
 from collections import defaultdict
 import pickle
@@ -18,14 +18,6 @@ base_path = os.getcwd()
 sys.path.append(base_path)
 
 import save_utils
-
-
-class options:
-    def __init__(self):
-        self.save_objs = False
-        self.load_objs = False
-
-opt = options()
 
 def split_df(raw_df, split):
     split_index = round(split * raw_df.shape[0])
@@ -43,7 +35,7 @@ def create_dict(id_list):
     print(len(id_dict))
     return id_dict
 
-def make_matrix(train_df, train_cid, train_mid)
+def make_matrix(train_df, train_cid, train_mid):
     # Make movie-by-user matrix to hold ratings
     train_ratings = np.zeros((train_cust_n, train_movies_n), np.short)
     for i in range(len(train_df)):
@@ -53,6 +45,23 @@ def make_matrix(train_df, train_cid, train_mid)
         train_ratings[customer_idx, movie_idx] = current[2]
     return train_ratings
 
+def preprocess_matrix(train_ratings):
+    train_processed_mov = np.zeros(train_ratings.shape)
+    for i in range(train_ratings.shape[1]):
+        rating_movie = train_ratings[:, i]
+        train_processed_mov[:, i] = rating_movie - np.mean(rating_movie)
+    return train_processed_mov
+
+
+
+
+class options:
+    def __init__(self):
+        self.save_objs = False
+        self.load_objs = False
+        self.split_dataset = True
+
+opt = options()
 
 if opt.save_objs or opt.load_objs:
     os.makedirs(base_path + '/obj/', exist_ok=True)
@@ -62,7 +71,10 @@ raw_df = pd.read_csv( base_path + "train.csv")
 raw_df = shuffle(raw_df)
 raw_df = raw_df.reset_index(drop=True)
 
-train_df, validate_df = split_df(raw_df, 0.8)
+if opt.split_dataset:
+    train_df, validate_df = split_df(raw_df, 0.8)
+else:
+    train_df, validate_df = split_df(raw_df, 0.0)
 
 train_customers = train_df['customer-id']
 train_cid_sorted = train_customers.sort_values()
@@ -79,7 +91,9 @@ print(train_movies_n, train_cust_n)
 train_ratings = make_matrix(train_df, train_cid, train_mid)
 
 if opt.save_objs:
-    save_obj(train_ratings, pre+'train_ratings')
+    save_utils.save_obj(train_ratings, pre+'train_ratings')
 if opt.load_objs:
-    train_ratings = load_obj(pre+'train_ratings')
+    train_ratings = save_utils.load_obj(pre+'train_ratings')
+
+
 
